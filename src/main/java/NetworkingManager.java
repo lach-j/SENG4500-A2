d195ec5d02937a9bcaf4800816cb90dc8afc7cca
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-09-27 11:38:04 +1000

    Make NetworkManager closeable Returns socket on createConnection

--- src/main/java/NetworkingManager.java
+++ src/main/java/NetworkingManager.java
@@ -1,42 +1,45 @@
-import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
-public class NetworkingManager {
+public class NetworkingManager implements Closeable {
 
     private final InetAddress broadcastAddress;
     private final int broadcastPort;
     private boolean connected = false;
+    private boolean isClient = false;
     private DatagramSocket udpSocket;
+    private Socket tcpSocket;
+    private ServerSocket serverSocket;
 
     public NetworkingManager(InetAddress address, int port) {
         this.broadcastAddress = address;
         this.broadcastPort = port;
     }
 
-    public void createConnection() {
+    public Socket createConnection() {
         Random rand = new Random();
         int gameCommandPort = rand.nextInt(1000) + 5000;
 
         var udpThread = new Thread(() -> broadcastGame(gameCommandPort));
         udpThread.start();
 
-        try (ServerSocket ss = new ServerSocket(gameCommandPort);
-                Socket currentConnection = ss.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(currentConnection.getInputStream()));
-                PrintWriter out = new PrintWriter(currentConnection.getOutputStream(), true)) {
+        try {
+            ServerSocket ss = new ServerSocket(gameCommandPort);
+            serverSocket = ss;
+            Socket currentConnection = ss.accept();
             System.out.println("CLIENT HAS CONNECTED!");
             connected = true;
+            this.tcpSocket = currentConnection;
+            isClient = false;
 
             // Close the connection early so socket doesnt need to timeout.
             udpSocket.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (IOException e) {
         }
+        return tcpSocket;
     }
 
     private void broadcastGame(int gameCommandPort) {
@@ -53,7 +56,8 @@ public class NetworkingManager {
                     udpSocket.receive(packet);
                 } catch (SocketTimeoutException e) {
                     if (!connected) {
-                        DatagramPacket outPacket = new DatagramPacket(requestBytes, requestBytes.length, broadcastAddress, broadcastPort);
+                        DatagramPacket outPacket = new DatagramPacket(requestBytes, requestBytes.length,
+                                broadcastAddress, broadcastPort);
                         udpSocket.send(outPacket);
                         System.out.printf("SENT: %s%n", request);
                     }
@@ -68,26 +72,46 @@ public class NetworkingManager {
 
                 if (s.split(":")[0].equals("NEW PLAYER") && gameCommandPort != Integer.parseInt(s.split(":")[1])) {
                     System.out.println("RECEIVED: " + s);
-                    connected = true;
                     var port = Integer.parseInt(s.split(":")[1]);
                     var addr = packet.getAddress();
                     joinExistingGame(port, addr);
-                    break;
+                    serverSocket.close();
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("UDP thread ended");
     }
 
     private void joinExistingGame(int port, InetAddress address) {
-        try (Socket s = new Socket(address, port);
-                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                PrintWriter out = new PrintWriter(s.getOutputStream(), true);) {
+        try {
+            Socket s = new Socket(address, port);
+            connected = true;
+            isClient = true;
             System.out.println("JOINING SOCKET ON PORT " + port);
+            this.tcpSocket = s;
         } catch (IOException ex) {
 
+        }
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public boolean isClient() {
+        return isClient;
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("NM CLOSED");
+        if (tcpSocket != null) {
+            tcpSocket.close();
+        }
+
+        if (serverSocket != null) {
+            serverSocket.close();
         }
     }
 }
