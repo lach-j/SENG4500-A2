8ace0d0d2933b6c12b9e193c1193af55a86d1548
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-09-27 11:36:49 +1000

    Update build script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 # Build app
-mvn clean install
+sudo -u $SUDO_USER mvn clean install
 
 # Push image
 docker build . -t seng4500:latest
 
 docker stop SENG4500-P1 SENG4500-P2
 docker rm SENG4500-P1 SENG4500-P2
-
-DOCKER_ARGS="--net seng4500net --entrypoint java -itd seng4500:latest -jar /usr/local/lib/A1.jar 172.18.255.255 5000"
-
-docker run --ip 172.18.0.21 --name SENG4500-P1 ${DOCKER_ARGS} &
-docker run --ip 172.18.0.22 --name SENG4500-P2 ${DOCKER_ARGS} & wait
 
 # Launch a terminator instance using the new layout
 terminator -l foo -p hold
